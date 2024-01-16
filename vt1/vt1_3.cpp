68f780bc9d3318b30b6c2c82df3cedc14f44922f
Jenna <s3saje00@students.osao.fi>
2024-01-16 22:08:54 +0200

    Tehtävä valmis (unohtunut askel lisätty)

--- vt1/vt1_3.cpp
+++ vt1/vt1_3.cpp
@@ -4,22 +4,24 @@
 
 using namespace std;
 
-void guesser(int maxNo);
+int guesser(int maxNo);
 
 int main()
 {
     int maxNo = 0;
     cout << "Anna korkein arvottava luku: ";
     cin >> maxNo;
-    guesser(maxNo);
+    int guesses = guesser(maxNo);
+    cout << "Tarvitsit " << guesses << " arvausta." << endl;
 
     return 0;
 }
 
-void guesser(int maxNo) {
+int guesser(int maxNo) {
     time_t currTime;
     srand(time(&currTime));
 
+    int guessNo = 0;
     int randNo = rand() % maxNo + 1;
 
     int guess = 0;
@@ -27,7 +29,7 @@ void guesser(int maxNo) {
     while (!guessTrue) {
         cout << "Arvaa luku: ";
         cin >> guess;
-
+        guessNo++;
         if (guess == randNo) {
             cout << "Arvasit oikein!" << endl;
             guessTrue = true;
@@ -39,4 +41,6 @@ void guesser(int maxNo) {
             cout << "Antamasi luku on liian suuri." << endl;
         }
     }
+
+    return guessNo;
 }
